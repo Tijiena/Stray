05f134240b43a6df918b96fddcb335c7e783b077
Tijiena <a13816106692@163.com>
2024-03-22 16:03:04 -0400

    Update InterpretArduino.cs

--- stray/Assets/Ardity/Scripts/InterpretArduino.cs
+++ stray/Assets/Ardity/Scripts/InterpretArduino.cs
@@ -9,6 +9,7 @@ public class InterpretArduinoS : MonoBehaviour
     public SerialController serialController;
     public float x, y, z;
     public int button1, button2;
+    public GameObject battery;
    
     void OnMessageArrived(string msg)
     {
@@ -20,7 +21,29 @@ public class InterpretArduinoS : MonoBehaviour
         z = float.Parse(newStrings[2]);
        button1 = int.Parse(newStrings[3]);
        button2= int.Parse(newStrings[4]);
-        
+
+        if (x >= 50 && x <= 235)
+        {
+            battery.transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f * Time.deltaTime, transform.position.z);
+            Debug.Log("Down");
+        }
+        if (y>= 2 && y <= 40)
+        {
+            battery.transform.position = new Vector3(transform.position.x + 0.5f * Time.deltaTime, transform.position.y, transform.position.z);
+            Debug.Log("Left");
+        }
+        if (y >= -25 && y<= 2)
+        {
+            battery.transform.position = new Vector3(transform.position.x - 0.5f * Time.deltaTime, transform.position.y, transform.position.z);
+            Debug.Log("Right");
+        }
+        if (z >= -170 && z <= 150)
+        {
+            battery.transform.position = new Vector3(transform.position.x - 0.5f * Time.deltaTime, transform.position.y, transform.position.z);
+            Debug.Log("Up");
+        }
+
+
 
         // Debug.Log(gameManager.currentPlush);
     }
