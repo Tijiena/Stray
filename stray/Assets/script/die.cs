82cb48c26a93e3b2f926b171260e930640d47dbb
Tijiena <a13816106692@163.com>
2024-03-27 00:06:39 -0400

    die script

---
+++ stray/Assets/script/die.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class die : MonoBehaviour
+{
+    private Vector3 startposition;
+    // Start is called before the first frame update
+    void Start()
+    {
+         startposition=transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (temperature.hotvalue>= 10)
+        {
+            transform.position=startposition;
+        }
+        if (temperature.coldvalue >= 10)
+        {
+            transform.position = startposition;
+        }
+    }
+}
